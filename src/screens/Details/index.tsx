a7a129c30e304accbc784b18b1e818b4b28d06ec
DailoFFelipe <dffva@ecomp.poli.br>
2021-12-07 09:57:21 -0300

    feat: add details screen

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,19 +1,65 @@
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import React, { useMemo } from "react";
 import { useRem } from "responsive-native";
 import { useTheme } from "styled-components";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { Container, MovieImage, Content } from "./styles";
+import { format, parseISO } from "date-fns";
+import { IMovie } from "../../services/types";
+
+import {
+  Container,
+  MovieImage,
+  Content,
+  Section,
+  Header,
+  TimeStarsText,
+  Date,
+  Title,
+  DateText,
+  Wrapper,
+  Genre,
+  GenreText,
+  BackButton,
+  Synopsis,
+  Text,
+} from "./styles";
+import { getMovieImg } from "../../services/useTrendingService";
+import GenreItem from "../../components/GenreItem";
+
+type Params = {
+  movie: IMovie;
+};
 
 const Details: React.FC = () => {
-  const { screen } = useTheme();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const { movie } = route.params as Params;
+
+  const { screen, colors } = useTheme();
   const rem = useRem();
+
+  const image = useMemo(
+    () => getMovieImg(movie.backdrop_path),
+    [movie.backdrop_path],
+  );
+
+  const releaseDate = useMemo(() => {
+    const dateParsed = parseISO(movie.release_date);
+
+    return format(dateParsed, `MMMM d',' yyyy`);
+  }, [movie.release_date]);
 
   return (
     <Container>
+      <BackButton onPress={goBack}>
+        <Entypo name="chevron-left" size={rem(2)} color={colors.gray} />
+      </BackButton>
+
       <MovieImage
         source={{
-          uri: `https://m.media-amazon.com/images/M/MV5BOTVhMzYxNjgtYzYwOC00MGIwLWJmZGEtMjgwMzgxMWUwNmRhXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_.jpg`,
+          uri: image,
         }}
       >
         <LinearGradient
@@ -27,7 +73,53 @@ const Details: React.FC = () => {
         />
       </MovieImage>
 
-      <Content>{/* <Title>Dora Aventureira</Title> */}</Content>
+      <Content>
+        <Section>
+          <Header>{movie.title}</Header>
+
+          <Wrapper>
+            <AntDesign
+              name="clockcircleo"
+              size={rem(0.688)}
+              color={colors.gray}
+            />
+
+            <Title>{movie.runtime} minutos</Title>
+
+            <AntDesign
+              name="star"
+              size={rem(0.688)}
+              color={colors.gray}
+              style={{
+                marginLeft: rem(1.25),
+              }}
+            />
+
+            <Text>{movie.vote_average} (TMDb)</Text>
+          </Wrapper>
+        </Section>
+
+        <Section>
+          <Wrapper>
+            <Date>
+              <Title>Release date</Title>
+              <Text>{releaseDate}</Text>
+            </Date>
+
+            {!!movie.genres && (
+              <Genre>
+                <Title>Genre</Title>
+                <GenreItem title={movie.genres[0].name} />
+              </Genre>
+            )}
+          </Wrapper>
+        </Section>
+
+        <Section>
+          <Title>Synopsis</Title>
+          <Text>{movie.overview}</Text>
+        </Section>
+      </Content>
     </Container>
   );
 };
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -1,8 +1,18 @@
+import { BorderlessButton } from "react-native-gesture-handler";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   background: ${({ theme }) => theme.colors.bg};
+`;
+
+export const BackButton = styled(BorderlessButton)`
+  position: absolute;
+
+  margin-top: ${({ theme }) =>
+    theme.screen.statusBarHeight + theme.screen.rem(1)}px;
+  margin-left: ${({ theme }) => theme.screen.rem(1)}px;
+  z-index: 9999;
 `;
 
 export const MovieImage = styled.ImageBackground`
@@ -15,4 +25,54 @@ export const MovieImage = styled.ImageBackground`
 
 export const Content = styled.View`
   flex: 1;
+
+  padding: 0 ${({ theme }) => theme.screen.rem(1)}px;
+`;
+
+export const Section = styled.View`
+  padding: ${({ theme }) => theme.screen.rem(1.5)}px 0;
+
+  border-bottom-width: 0.2px;
+  border-bottom-color: ${({ theme }) => theme.colors.border};
+`;
+
+export const Header = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${({ theme }) => theme.screen.rem(1.25, true)}px;
+  color: ${({ theme }) => theme.colors.white};
+
+  margin-bottom: ${({ theme }) => theme.screen.rem(0.5)}px;
+`;
+
+export const Date = styled.View`
+  margin-right: ${({ theme }) => theme.screen.rem(2)}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${({ theme }) => theme.screen.rem(0.975, true)}px;
+  color: ${({ theme }) => theme.colors.white};
+
+  margin-bottom: ${({ theme }) => theme.screen.rem(0.85)}px;
+`;
+
+export const GenreText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${({ theme }) => theme.screen.rem(0.975, true)}px;
+  color: ${({ theme }) => theme.colors.white};
+
+  margin-bottom: ${({ theme }) => theme.screen.rem(0.5)}px;
+`;
+
+export const Wrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Genre = styled.View``;
+
+export const Text = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${({ theme }) => theme.screen.rem(0.688, true)}px;
+  color: ${({ theme }) => theme.colors.gray};
 `;
